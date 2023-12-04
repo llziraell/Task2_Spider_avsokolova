fa1dc9e85cf9074a37a4e7f00f9f01b11b5fa507
llziraell <98565914+llziraell@users.noreply.github.com>
2023-12-04 12:21:48 +0300

    add movable point

--- widget.cpp
+++ widget.cpp
@@ -1,5 +1,70 @@
 #include "widget.h"
 
-Widget::Widget(QWidget *parent) : QWidget(parent) {}
+#include <QRandomGenerator>
+#include <QKeyEvent>
+#include <QPainter>
+#include <QPoint>
+
+Widget::Widget(QWidget *parent) : QWidget(parent) {
+    windowRect_ = rect();
+    SetSpiderPosition();
+}
 
 Widget::~Widget() {}
+
+void Widget::SetSpiderPosition() {
+        int width = this->width();
+        int height = this->height();
+
+        int x = 0, y = 0;
+
+        x = QRandomGenerator::global()->bounded(width);
+        y = QRandomGenerator::global()->bounded(height);
+
+        spiderPosition_.setX(x);
+        spiderPosition_.setY(y);
+        isDrawingWeb_ = true;
+}
+
+void Widget::keyPressEvent(QKeyEvent *event) {
+    if (event->key() == Qt::Key_Up) {
+        spiderPosition_.setY(spiderPosition_.y() - shift);
+    }
+    if (event->key() == Qt::Key_Down) {
+        spiderPosition_.setY(spiderPosition_.y() + shift);
+    }
+    if (event->key() == Qt::Key_Left) {
+        spiderPosition_.setX(spiderPosition_.x() - shift);
+    }
+    if (event->key() == Qt::Key_Right) {
+        spiderPosition_.setX(spiderPosition_.x() + shift);
+    }
+    repaint();
+}
+
+void Widget::resizeEvent(QResizeEvent *event) {
+    Q_UNUSED(event);
+    windowRect_ = rect();
+}
+
+void Widget::paintEvent(QPaintEvent *event) {
+    Q_UNUSED(event);
+
+    // Создаем экземпляр класса для низкоуровневого рисования
+    QPainter painter(this);
+    painter.setRenderHint(QPainter::Antialiasing);  // Добавляем сглаживание
+
+    if (isDrawingWeb_) {  // Рисуем паутину и паука при зажатой левой кнопке мыши
+        //DrawWeb(&painter);
+        DrawSpider(&painter);
+    }
+}
+
+void Widget::DrawSpider(QPainter *painter) {
+    int width = 5;
+    int height = 5;
+
+    painter->setBrush(QBrush(Qt::red, Qt::SolidPattern));
+    painter->drawEllipse(spiderPosition_, width, height);
+}
+
--- widget.h
+++ widget.h
@@ -2,6 +2,8 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QPoint>
+#include <QRect>
 
 class Widget : public QWidget {
   Q_OBJECT
@@ -9,5 +11,23 @@ class Widget : public QWidget {
  public:
   Widget(QWidget *parent = nullptr);
   ~Widget();
+
+ protected:
+  virtual void resizeEvent(QResizeEvent *event);
+  virtual void keyPressEvent(QKeyEvent *event);
+  virtual void paintEvent(QPaintEvent *event);
+
+ private slots:
+  void SetSpiderPosition();
+
+private:
+    void DrawEllipse(QPainter *painter);
+    void DrawSpider(QPainter *painter);
+    void ShowMousePosition();
+
+    QPoint spiderPosition_;
+    QRect windowRect_;
+    bool isDrawingWeb_;
+    int shift = 10;
 };
 #endif  // WIDGET_H
